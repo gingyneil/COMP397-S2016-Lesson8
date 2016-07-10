879252bb4cd0266e0174feb2e5c67006955a8ced
gingyneil <neil.reading.nr@gmail.com>
2016-07-10 10:51:44 -0400

    added example createJS

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,6 +1,26 @@
+///<reference path="reference.ts"/>
+// make a reference to the canvas element
+var canvas = document.getElementById("canvas");
+// create a ference to a stage container
+var stage;
+var helloLabel;
 function init() {
-    console.log("Game Started now hey!");
-    console.log("did git work");
+    stage = new createjs.Stage(canvas); // instantiates the stage container
+    createjs.Ticker.framerate = 60; // frame rate
+    createjs.Ticker.on("tick", gameLoop); // create an event listener for the tick event
+    main(); // call the main game function 
+}
+function gameLoop() {
+    helloLabel.rotation += 5;
+    stage.update(); // refreshes the page
+}
+function main() {
+    helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * .5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * .5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
 }
 window.addEventListener("load", init);
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,11 +1,34 @@
+///<reference path="reference.ts"/>
 
-	
-	
+// make a reference to the canvas element
+let canvas:HTMLElement = document.getElementById("canvas");	
+// create a ference to a stage container
+let stage:createjs.Stage;
+
+let helloLabel:createjs.Text;
 
 function init():void {
-console.log("Game Started now hey!");
-console.log("did git work");
+    stage = new createjs.Stage(canvas);// instantiates the stage container
+    createjs.Ticker.framerate = 60; // frame rate
+    createjs.Ticker.on("tick", gameLoop);    // create an event listener for the tick event
+    main(); // call the main game function 
+}
+
+function gameLoop():void {
+    helloLabel.rotation += 5;
+    
+    
+    stage.update(); // refreshes the page
 
 }
 
+
+function main():void {
+    helloLabel = new createjs.Text("Hello world!", "40px Arial", "#00000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * .5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * .5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
+}
 window.addEventListener("load", init);
